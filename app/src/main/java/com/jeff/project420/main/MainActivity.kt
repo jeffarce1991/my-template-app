c903ced24bf1b9b4846fb6f35b042ad3edfeb54c
Jeff Arce <jeff.arce@upraxis.com>
2020-04-19 23:11:56 +0800

    Moved MainActivitty to main package.

---
+++ app/src/main/java/com/jeff/project420/main/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.jeff.project420.main
+
+import android.app.ProgressDialog
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.jeff.project420.R
+import com.jeff.project420.adapter.CustomAdapter
+import com.jeff.project420.database.local.Photo
+import com.jeff.project420.database.room.PhotoDatabase
+import com.jeff.project420.databinding.ActivityMainBinding
+import com.jeff.project420.main.presenter.MainActivityPresenter
+
+
+class MainActivity : AppCompatActivity(), MainActivityPresenter.View {
+    private lateinit var adapter: CustomAdapter
+    private lateinit var progressDialog: ProgressDialog
+
+    private lateinit var presenter: MainActivityPresenter
+    lateinit var mainBinding : ActivityMainBinding
+    lateinit var photoDatabase: PhotoDatabase
+
+    lateinit var photos : List<Photo>
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        presenter = MainActivityPresenter(this, this)
+
+        presenter.getPhotoList()
+    }
+
+        /*Method to generate List of data using RecyclerView with custom com.project.retrofit.adapter*/
+        override fun generateDataList(photos: List<Photo>) {
+            adapter = CustomAdapter(this, photos)
+            val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this@MainActivity)
+            mainBinding.customRecyclerView.layoutManager = layoutManager
+            mainBinding.customRecyclerView.adapter = adapter
+        }
+
+    override fun hideProgress() {
+        progressDialog.dismiss()
+    }
+
+    override fun showProgress() {
+        progressDialog = ProgressDialog.show(
+        this,
+        "Retrofit",
+        "Loading...")
+    }
+
+    /*private fun hasConnection(): Boolean {
+        val connectivityManager = applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE)
+                as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+
+        return networkInfo != null && (networkInfo.state == CONNECTED
+                || networkInfo.state == CONNECTING)
+    }*/
+}
