07f465d421dc35124fb8db6b3fb5d70327acc8a9
Jeff Arce <jeff.arce@upraxis.com>
2020-05-01 07:35:54 +0800

    Add loadAllFromLocal() on PhotoLoader.

--- app/src/main/java/com/jeff/project420/supplychain/photo/DefaultPhotoLoader.kt
+++ app/src/main/java/com/jeff/project420/supplychain/photo/DefaultPhotoLoader.kt
@@ -1,15 +1,18 @@
 package com.jeff.project420.supplychain.photo
 
+import androidx.room.EmptyResultSetException
+import com.jeff.project420.Constants.DaoExceptions.ERROR_EMPTY_RESULT
+import com.jeff.project420.Constants.DaoExceptions.ERROR_NULL_RESULT
 import com.jeff.project420.database.local.Photo
 import com.jeff.project420.database.usecase.local.loader.PhotoLocalLoader
 import com.jeff.project420.database.usecase.local.saver.PhotoLocalSaver
 import com.jeff.project420.main.mapper.PhotoDtoToPhotoMapper
-import com.jeff.project420.webservices.dto.PhotoDto
-import com.jeff.project420.webservices.internet.DefaultRxInternet
+import com.jeff.project420.utilities.exception.EmptyResultException
 import com.jeff.project420.webservices.internet.RxInternet
 import com.jeff.project420.webservices.usecase.loader.PhotoRemoteLoader
 import io.reactivex.Observable
 import io.reactivex.Single
+import java.lang.NullPointerException
 import javax.inject.Inject
 
 class DefaultPhotoLoader @Inject
@@ -30,6 +33,19 @@ constructor(private val remoteLoader: PhotoRemoteLoader,
         //return session.activeAdministeredAccount()
         //.map { administeredAccount -> administeredAccount.tenantId }
         //.flatMap { tenantId -> remoteLoader.loadAvailableExpenses(tenantId) }
+    }
+
+    override fun loadAllFromLocal(): Single<List<Photo>> {
+        return rxInternet.notConnected()
+            .andThen(localLoader.loadAll())
+            /*.flatMap {
+                when {
+                    it.isEmpty() -> Single.error(
+                        Throwable(EmptyResultException()))
+                    else -> Single.just(it)
+                }
+            }*/
+            .flatMap { photos -> Single.just(photos)}
     }
 
 }
--- app/src/main/java/com/jeff/project420/supplychain/photo/PhotoLoader.kt
+++ app/src/main/java/com/jeff/project420/supplychain/photo/PhotoLoader.kt
@@ -7,4 +7,6 @@ import io.reactivex.Single
 interface PhotoLoader {
 
     fun loadAll(): Single<List<Photo>>
+
+    fun loadAllFromLocal(): Single<List<Photo>>
 }
